a39c70d203595c2781658fcf311d6d29e68845da
giovaneneves7 <autornekoyasha@gmail.com>
2024-08-25 20:13:52 -0300

    style: Muda cores da home

---
+++ lib/features/home/screens/home_offer_screen.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class HomeOfferScreen extends StatefulWidget {
+  const HomeOfferScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeOfferScreenState createState() => _HomeOfferScreenState();
+}
+
+class _HomeOfferScreenState extends State<HomeOfferScreen> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+      appBar: AppBar(
+        title: Text('Stateful Screen'),
+      ),
+      body:SingleChildScrollView(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search, color: Theme.of(context).iconTheme.color),
+                hintText: 'Pesquise Qualquer Coisa...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                image: DecorationImage(
+                  image: AssetImage('assets/images/promo.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                'Bom fim de semana\n25% OFF\n*para Todos os Itens*',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text("Categorias", style: Theme.of(context).textTheme.headline6),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                _buildCategoryCard(Icons.restaurant, 'Alimentos'),
+                _buildCategoryCard(Icons.devices_other, 'Equipamentos'),
+                _buildCategoryCard(Icons.checkroom, 'Roupas'),
+                _buildCategoryCard(Icons.chair, 'Móveis'),
+              ],
+            ),
+            SizedBox(height: 20),
+            Text("Popular", style: Theme.of(context).textTheme.headline6),
+            SizedBox(height: 10),
+            _buildPopularItem(),
+            SizedBox(height: 10),
+            _buildPopularItem(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCategoryCard(IconData icon, String title) {
+    return Column(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Icon(
+                  icon, 
+                  size: 30, 
+                  color: Theme.of(context).colorScheme.onPrimary,
+                ),
+        ),
+        SizedBox(height: 5),
+        //Text(title, style: Theme.of(context).textTheme.bodyText1),
+      ],
+    );
+  }
+
+  Widget _buildPopularItem() {
+    return Container(
+      height: 100,
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.surface,
+        borderRadius: BorderRadius.circular(8),
+        image: DecorationImage(
+          image: AssetImage('assets/images/promo.jpg'),
+          fit: BoxFit.cover,
+        ),
+      ),
+      alignment: Alignment.center,
+      child: Text(
+        'Popular Item',
+        style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.white),
+      ),
+    );
+  }
+}
--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -6,6 +6,7 @@ import 'package:corre_aqui/features/offer-map/screens/offer_map_screen.dart';
 import 'package:corre_aqui/features/profile/screens/profile_screen.dart';
 import 'package:corre_aqui/features/favorite/screens/favorite_screen.dart';
 import 'package:corre_aqui/features/notification/screens/notification_screen.dart';
+import 'package:corre_aqui/features/home/screens/home_offer_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -39,69 +40,18 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         actions: [
           IconButton(
-            icon: Icon(Icons.notifications, color: Theme.of(context).iconTheme.color),
+            icon: Icon(Icons.notifications, color: Theme.of(context).colorScheme.onPrimary),
             onPressed: () {
               Get.toNamed(RouteHelper.getNotificationScreen());
             },
           ),
         ],
       ),
-      body: SingleChildScrollView(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search, color: Theme.of(context).iconTheme.color),
-                hintText: 'Pesquise Qualquer Coisa...',
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                image: DecorationImage(
-                  image: AssetImage('assets/images/promo.jpg'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              alignment: Alignment.center,
-              child: Text(
-                'Bom fim de semana\n25% OFF\n*para Todos os Itens*',
-                textAlign: TextAlign.center,
-                style: Theme.of(context).textTheme.headline6?.copyWith(color: Colors.white),
-              ),
-            ),
-            SizedBox(height: 20),
-            Text("Categorias", style: Theme.of(context).textTheme.headline6),
-            SizedBox(height: 10),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                _buildCategoryCard(Icons.restaurant, 'Alimentos'),
-                _buildCategoryCard(Icons.devices_other, 'Equipamentos'),
-                _buildCategoryCard(Icons.checkroom, 'Roupas'),
-                _buildCategoryCard(Icons.chair, 'Móveis'),
-              ],
-            ),
-            SizedBox(height: 20),
-            Text("Popular", style: Theme.of(context).textTheme.headline6),
-            SizedBox(height: 10),
-            _buildPopularItem(),
-            SizedBox(height: 10),
-            _buildPopularItem(),
-          ],
-        ),
-      ),
+      body: 
       IndexedStack(
         index: _selectedIndex,
         children: [
-          HomeScreen(), // Tela principal
+          HomeOfferScreen(), // Tela principal
           NotificationScreen(), // Tela que será substituida futuramente
           OfferMapScreen(), // Tela do Mapa
           FavoriteScreen(), // Tela de Favoritos
@@ -115,40 +65,4 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  Widget _buildCategoryCard(IconData icon, String title) {
-    return Column(
-      children: [
-        Container(
-          width: 60,
-          height: 60,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.surface,
-            borderRadius: BorderRadius.circular(8),
-          ),
-          child: Icon(icon, size: 30, color: Theme.of(context).colorScheme.primary),
-        ),
-        SizedBox(height: 5),
-        Text(title, style: Theme.of(context).textTheme.bodyText1),
-      ],
-    );
-  }
-
-  Widget _buildPopularItem() {
-    return Container(
-      height: 100,
-      decoration: BoxDecoration(
-        color: Theme.of(context).colorScheme.surface,
-        borderRadius: BorderRadius.circular(8),
-        image: DecorationImage(
-          image: AssetImage('assets/images/promo.jpg'),
-          fit: BoxFit.cover,
-        ),
-      ),
-      alignment: Alignment.center,
-      child: Text(
-        'Popular Item',
-        style: Theme.of(context).textTheme.bodyText1?.copyWith(color: Colors.white),
-      ),
-    );
-  }
 }
