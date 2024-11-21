5fe8129d6aa228927baa49dec3d1cd0cb7e64e37
giovaneneves7 <autornekoyasha@gmail.com>
2024-11-21 08:04:48 -0300

    feat(auth): Traduz tela de signin

--- lib/features/auth/screens/auth_gate.dart
+++ lib/features/auth/screens/auth_gate.dart
@@ -8,44 +8,47 @@ import 'package:flutter/material.dart';
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
 
-@override
- Widget build(BuildContext context) {
-   return StreamBuilder<User?>(
-     stream: FirebaseAuth.instance.authStateChanges(),
-     builder: (context, snapshot) {
-       if (!snapshot.hasData) {
-         return SignInScreen(
-           providers: [
-             EmailAuthProvider()
-           ],
-           headerBuilder: (context, constraints, shrinkOffset) {
-             return Padding(
-               padding: const EdgeInsets.all(20),
-               child: AspectRatio(
-                 aspectRatio: 1,
-                 child: Image.asset(Images.logo),
-               ),
-             );
-           },
-           subtitleBuilder: (context, action) {
-             return Padding(
-               padding: const EdgeInsets.symmetric(vertical: 8.0),
-               child: action == AuthAction.signIn
-                   ? const Text('Welcome to FlutterFire, please sign in!')
-                   : const Text('Welcome to Flutterfire, please sign up!'),
-             );
-           },
-           footerBuilder: (context, action) {
-             return const Padding(
-               padding: EdgeInsets.only(top: 16),
-               child: Text(
-                 'By signing in, you agree to our terms and conditions.',
-                 style: TextStyle(color: Colors.grey),
-               ),
-             );
-           },
-         );
-       }
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider()
+            ],
+            localizations: const {
+              'signIn': 'Entrar', 
+            },
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return Padding(
+                padding: const EdgeInsets.all(20),
+                child: AspectRatio(
+                  aspectRatio: 1,
+                  child: Image.asset(Images.logo),
+                ),
+              );
+            },
+            subtitleBuilder: (context, action) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: action == AuthAction.signIn
+                    ? const Text('Bem-vindo ao Corre Aqui! Por favor, faça seu login')
+                    : const Text('Bem-vindo ao Corre Aqui! Por favor, faça seu registro'),
+              );
+            },
+            footerBuilder: (context, action) {
+              return const Padding(
+                padding: EdgeInsets.only(top: 16),
+                child: Text(
+                  'Ao se inscrever, você concorda com os nossos termos e condições.',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              );
+            },
+          );
+        }
 
        return HomeScreen();
      },
