72907fefc7846300336d504d93f518c0df24bced
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-01 20:36:48 -0300

    feat(api): Edita api client

---
+++ lib/api/supabase_api_client.dart
@@ -0,0 +1,82 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseApiClient {
+
+  	final SupabaseClient client;
+  	final String authToken;
+	final SharedPreferences sharedPreferences;
+
+  	SupabaseApiClient({required this.client, required this.authToken, required this.sharedReferences});
+
+  	Map<String, String> _getHeaders() {
+    	return {
+      	'Authorization': 'Bearer $authToken',
+      	'Content-Type': 'application/json',
+    	};
+  	}
+
+  Future<List<Map<String, dynamic>>> getData(String table, {Map<String, dynamic>? filters}) async {
+    
+    try {
+      final query = client.from(table).select();
+      filters?.forEach((key, value) {
+        query.eq(key, value);
+      });
+
+      final response = await query.execute();
+      if (response.error != null) {
+        throw Exception('Erro: ${response.error!.message}');
+      }
+      return response.data as List<Map<String, dynamic>>;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Método genérico para inserir dados
+  Future<void> postData(String table, Map<String, dynamic> body) async {
+    try {
+      final response = await client.from(table).insert(body).execute();
+      if (response.error != null) {
+        throw Exception('Erro: ${response.error!.message}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Método genérico para atualizar dados
+  Future<void> putData(String table, Map<String, dynamic> body, Map<String, dynamic> filters) async {
+    try {
+      final query = client.from(table).update(body);
+      filters.forEach((key, value) {
+        query.eq(key, value);
+      });
+
+      final response = await query.execute();
+      if (response.error != null) {
+        throw Exception('Erro: ${response.error!.message}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // Método genérico para excluir dados
+  Future<void> deleteData(String table, Map<String, dynamic> filters) async {
+    try {
+      final query = client.from(table).delete();
+      filters.forEach((key, value) {
+        query.eq(key, value);
+      });
+
+      final response = await query.execute();
+      if (response.error != null) {
+        throw Exception('Erro: ${response.error!.message}');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
--- lib/helper/get_di.dart
+++ lib/helper/get_di.dart
@@ -1,8 +1,16 @@
+import 'package:corre_aqui/api/supabase_api_client.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<Map<String, Map<String, String>>> init() async {
 
+	final supabaseClient = SupabaseApiClient(
+    	client: Supabase.instance.client,
+    	authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA',
+  	);
 	final sharedPreferences = await SharedPreferences.getInstance();
+
 	Get.lazyPut(() => sharedPreferences);
+	Get.lazyPut(() => SupabaseApiClient(client: supabaseClient, authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA', sharedPreferences: Get.find()));
+
 }
