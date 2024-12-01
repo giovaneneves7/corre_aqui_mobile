5de9bd01106b492f5a89cc76f70a6df9c79db7af
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-01 11:01:59 -0300

    update: Adiciona suporte ara supabase

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:corre_aqui/theme/custom_theme.dart';
 import 'package:corre_aqui/helper/route_helper.dart';
 import 'package:corre_aqui/util/app_translations.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -15,7 +16,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
-  );
+    );
+
+    await Supabase.initialize(
+      url: 'https://mpyyooswcudkgouiomuo.supabase.co',
+      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA',
+    );
 
   runApp(MyApp());
 }
