a7f4c7b291d40f3c4e8629d85ccb6a1c8605249f
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-02 07:53:10 -0300

    feat(banner): Cria controller

---
+++ lib/features/banner/controllers/banner_controller.dart
@@ -0,0 +1,31 @@
+import 'package:corre_aqui/features/banner/domain/models/banner.dart';
+import 'package:corre_aqui/features/banner/domain/services/banner_service_interface.dart';
+import 'package:get/get.dart';
+
+/**
+* @author Giovane Neves
+* @since v0.0.1
+*/
+class BannerController extends GetxController implements GetxService {
+
+
+	final BannerServiceInterface bannerServiceInterface;
+
+	BannerController({required this.bannerServiceInterface});
+
+
+	List<Banner>? _bannerList;
+	List<Banner>? get bannerList => _bannerList;
+
+	Future<void> getBannerList() async{
+
+		try {
+      		_bannerList = await bannerServiceInterface.getBannerList();
+      		update();
+    	} catch (e) {
+      		print('Erro ao buscar banners: $e');
+    	}
+
+	}
+
+}
