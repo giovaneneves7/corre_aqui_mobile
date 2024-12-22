f4ff26d935a9c24a0eafd86b362a08f9164b63af
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-21 21:16:00 -0300

    udate: Comentando código

--- lib/helper/get_di.dart
+++ lib/helper/get_di.dart
@@ -16,17 +16,17 @@ Future<Map<String, Map<String, String>>> init() async {
 	final sharedPreferences = await SharedPreferences.getInstance();
 
 	Get.lazyPut(() => sharedPreferences);
-	Get.lazyPut(() => SupabaseApiClient(client: supabaseClient, authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA', sharedPreferences: Get.find()));
+	//Get.lazyPut(() => SupabaseApiClient(client: supabaseClient, authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA', sharedPreferences: Get.find()));
 
 	// Repositories
-	BannerRepositoryInterface bannerRepositoryInterface = BannerRepository(apiClinet: Get.find());
-	Get.lazyPut(() => bannerRepositoryInterface);
+	//BannerRepositoryInterface bannerRepositoryInterface = BannerRepository(apiClinet: Get.find());
+	//Get.lazyPut(() => bannerRepositoryInterface);
 
 	// Services
-	BannerServiceInterface bannerServiceInterface = BannerService(bannerRepositoryInterface: Get.find());
-	Get.lazyPut(() => bannerServiceInterface);
+	//BannerServiceInterface bannerServiceInterface = BannerService(bannerRepositoryInterface: Get.find());
+	//Get.lazyPut(() => bannerServiceInterface);
 
 	// Controllers
-	Get.lazyPut(() => BannerController(bannerServiceInterface: bannerServiceInterface));
+	//Get.lazyPut(() => BannerController(bannerServiceInterface: bannerServiceInterface));
 	
 }
