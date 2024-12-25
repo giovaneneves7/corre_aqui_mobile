da345991cefc226a5f4bd48d9f947b72f08e15b1
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-25 18:55:11 -0300

    update: Adiciona repositório de store

--- lib/helper/get_di.dart
+++ lib/helper/get_di.dart
@@ -2,6 +2,8 @@ import 'package:corre_aqui/api/supabase_api_client.dart';
 import 'package:corre_aqui/features/banner/controllers/banner_controller.dart';
 import 'package:corre_aqui/features/banner/domain/repositories/banner_repository_interface.dart';
 import 'package:corre_aqui/features/banner/domain/repositories/banner_repository.dart';
+import 'package:corre_aqui/features/store/domain/repositories/store_repository';
+import 'package:corre_aqui/features/store/domain/repositories/store_repository_interface';
 import 'package:corre_aqui/features/banner/domain/services/banner_service.dart';
 import 'package:corre_aqui/features/banner/domain/services/banner_service_interface.dart';
 import 'package:get/get.dart';
@@ -16,11 +18,15 @@ Future<Map<String, Map<String, String>>> init() async {
 	final sharedPreferences = await SharedPreferences.getInstance();
 
 	Get.lazyPut(() => sharedPreferences);
-	//Get.lazyPut(() => SupabaseApiClient(client: supabaseClient, authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA', sharedPreferences: Get.find()));
+	Get.lazyPut(() => SupabaseApiClient(client: supabaseClient, authToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1weXlvb3N3Y3Vka2dvdWlvbXVvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwNTk5OTgsImV4cCI6MjA0ODYzNTk5OH0.zjQSRDm22Q8A-hCGyLLd9MObB9pIMCmf9bKg9aaE6AA', sharedPreferences: Get.find()));
 
 	// Repositories
-	//BannerRepositoryInterface bannerRepositoryInterface = BannerRepository(apiClinet: Get.find());
-	//Get.lazyPut(() => bannerRepositoryInterface);
+	StoreRepositoryInterface storeRepository = StoryRepository(apiClient: Get.find());
+	Get.lazyPut(() => storyRepository);
+	
+	//BannerRepositoryInterface bannerRepository = BannerRepository(apiClinet: Get.find());
+	//Get.lazyPut(() => bannerRepository);
+
 
 	// Services
 	//BannerServiceInterface bannerServiceInterface = BannerService(bannerRepositoryInterface: Get.find());
