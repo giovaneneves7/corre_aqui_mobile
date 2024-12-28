c0bd4486680294c4ecb6041400cab74dbd6cb402
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-28 15:03:21 -0300

    update: Modulariza widget de cetegorias

--- lib/features/home/screens/home_offer_screen.dart
+++ lib/features/home/screens/home_offer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:corre_aqui/features/home/widgets/banner_carousel.dart';
+import 'package:corre_aqui/features/home/widgets/category_list.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -72,15 +73,7 @@ class HomeOfferScreen extends StatelessWidget {
               const SizedBox(height: 16),
 
               // Categories
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  _buildCategoryButton("Todos", isSelected: true),
-                  _buildCategoryButton("Academia"),
-                  _buildCategoryButton("Roupas"),
-                  _buildCategoryButton("Supermercado"),
-                ],
-              ),
+              CategoryList(),
               const SizedBox(height: 16),
 
               // Parceiros
@@ -121,23 +114,6 @@ class HomeOfferScreen extends StatelessWidget {
         ],
         currentIndex: 0,
         onTap: (index) {},
-      ),
-    );
-  }
-
-  Widget _buildCategoryButton(String title, {bool isSelected = false}) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      decoration: BoxDecoration(
-        color: isSelected ? Colors.red : Colors.grey.shade200,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Text(
-        title,
-        style: TextStyle(
-          color: isSelected ? Colors.white : Colors.black,
-          fontWeight: FontWeight.w500,
-        ),
       ),
     );
   }
---
+++ lib/features/home/widgets/category_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+/**
+* @author Giovane Neves
+* @since v0.0.1
+*/
+class CategoryList extends StatelessWidget{
+
+
+	@override
+	Widget build(){
+
+		return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+	            _buildCategoryButton("Todos", isSelected: true),
+	            _buildCategoryButton("Academia"),
+	            _buildCategoryButton("Roupas"),
+	            _buildCategoryButton("Supermercado"),
+            ],
+        );
+
+	}
+
+	Widget _buildCategoryButton(String title, {bool isSelected = false}) {
+	    
+	    return Container(
+	      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+	      decoration: BoxDecoration(
+	        color: isSelected ? Colors.red : Colors.grey.shade200,
+	        borderRadius: BorderRadius.circular(16),
+	      ),
+	      child: Text(
+	        title,
+	        style: TextStyle(
+	          color: isSelected ? Colors.white : Colors.black,
+	          fontWeight: FontWeight.w500,
+	        ),
+	      ),
+	    );
+	    
+	}
+}
