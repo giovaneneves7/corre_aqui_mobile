870f7d22e193edaa1a1370e1e5f5ac2808128333
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-31 20:16:59 -0300

    fix: Resolve erro de named parameter

--- lib/features/category/domain/models/category.dart
+++ lib/features/category/domain/models/category.dart
@@ -8,7 +8,7 @@ class Category{
 	String name;
 	String image;
 
-	Category({this.name, this.image});
+	Category({required this.name, required this.image});
 
 	factory Category.fromJson(Map<String, dynamic> json) {
 	    return Category(
