2c0edf569bbbb18ac19fe72f7d524fb0e7e90ef8
giovaneneves7 <autornekoyasha@gmail.com>
2024-12-31 15:10:08 -0300

    fix: Resolve erro de retorno

--- lib/features/home/widgets/new_stores_section.dart
+++ lib/features/home/widgets/new_stores_section.dart
@@ -4,45 +4,42 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 /**
-* @author Giovane Neves
-* @since v0.0.1
-*/
-class NewStoresSection extends StatelessWidget{
-
-	@override
-	Widget build(BuildContext context){
-
-		return GetBuilder<StoreController>(
-			builder: (controller){
-
-				if (controller.stores.isEmpty) {
-          			return const Center(
-            			child: CircularProgressIndicator(), 
-          		);
-
-          		return Column(
-          			crossAxisAlignment: CrossAxisAlignment.start,
-          			children: [
-	            		const Text(
-	              			"Novas Lojas",
-	             	 		style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-	            		),
-	            		const SizedBox(height: 16),
-	            		SingleChildScrollView(
-	              			scrollDirection: Axis.horizontal, 
-	              			child: Row(
-		                		children: controller.stores.map((store) {
-			                  		return Padding(
-			                    		padding: const EdgeInsets.only(right: 16.0), 
-			                    		child: StoreCardTemplate(store: store),
-			                  		);
-		                		}).toList(),
-	              			),
-	            		),
-          			],
-        		);	
-        	}
-		});
-	}
-
+ * @author Giovane Neves
+ * @since v0.0.1
+ */
+class NewStoresSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<StoreController>(
+      builder: (controller) {
+        if (controller.stores.isEmpty) {
+          return const Center(
+            child: CircularProgressIndicator(), 
+          );
+        } else {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "Novas Lojas",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 16),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal, // Rola horizontalmente
+                child: Row(
+                  children: controller.stores.map((store) {
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 16.0), // Espaçamento entre os cards
+                      child: StoreCardTemplate(store: store),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ],
+          );
+        }
+      },
+    );
+  }
 }
