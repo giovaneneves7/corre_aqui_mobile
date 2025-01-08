9af0f9fa13a052506eb8e0f6ff5ab45c516e5f63
giovaneneves7 <autornekoyasha@gmail.com>
2025-01-08 20:00:33 -0300

    fix: Resolve erro de sintaxe

--- lib/features/home/widgets/hot_offers_widget.dart
+++ lib/features/home/widgets/hot_offers_widget.dart
@@ -8,61 +8,58 @@ import 'package:get/get.dart';
 * @author Giovane Neves
 * @since v0.0.1
 */
-class HotOffersWidget extends StatelessWidget{
-
-
-	@override
-	Widget build(BuildContext context){
-
-		return GetBuilder<OfferController>(
-			builder: (offerController){
-				return Column(
-					crossAxisAlignment: CrossAxisAlignment.start,
-					children: [
-						Padding(
-							padding: const EdgeInsets.symmetric(horizontal: 6.0),
-							child: Row(
-								mainAxisAlignment: MainAxisAlignment.spaceBetween,
-								children: [
-									Text(
-				                      "Maiores Descontos",
-				                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-				                    ),
-				                    TextButton(
-				                      onPressed: () {},
-				                      child: Text(
-				                        "Ver Mais",
-				                        style: TextStyle(color: Colors.red),
-				                      ),
-				                    ),
-								],
-							),
-							SizedBox(
-								height: 200,
-								child: ListView.builder(
-									scrollDirection: Axis.horizontal,
-									itemCount: offerController.hightDiscountOfferList.length,
-									itemBuilder: (context, index) {
-						            	final offer = offerController.hightDiscountOfferList[index];
-						                return Padding(
-						                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-						                    child: SizedBox(
-						                        width: 180,  
-						                        child: GestureDetector(
-							                        onTap: () {
-							                            Get.toNamed(RouteHelper.getOfferDetailsScreen(offerId: offer.id));
-							                        },
-							                        child: OfferCardTemplate(offer: offer, isFromHome: true),
-						                        ),
-						                    ),
-						                );
-						          	},
-								),
-							),
-						),
-					],
-				);
-			}
-		);
-	}
+class HotOffersWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetBuilder<OfferController>(
+      builder: (offerController) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 6.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Maiores Descontos",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Ver Mais",
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: offerController.hightDiscountOfferList.length,
+                itemBuilder: (context, index) {
+                  final offer = offerController.hightDiscountOfferList[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: SizedBox(
+                      width: 180,
+                      child: GestureDetector(
+                        onTap: () {
+                          Get.toNamed(RouteHelper.getOfferDetailsScreen(offerId: offer.id));
+                        },
+                        child: OfferCardTemplate(offer: offer, isFromHome: true),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
 }
